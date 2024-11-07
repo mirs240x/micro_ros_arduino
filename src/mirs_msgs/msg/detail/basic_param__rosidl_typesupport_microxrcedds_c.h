52c42efd3177de9646361942ad55224b9945cc5f
oumuika <oumuika@gmail.com>
2024-11-07 18:06:10 +0900

    Re compiled

---
+++ src/mirs_msgs/msg/detail/basic_param__rosidl_typesupport_microxrcedds_c.h
@@ -0,0 +1,39 @@
+// generated from rosidl_typesupport_microxrcedds_c/resource/idl__rosidl_typesupport_c.h.em
+// with input from mirs_msgs:msg/BasicParam.idl
+// generated code does not contain a copyright notice
+#ifndef MIRS_MSGS__MSG__BASIC_PARAM__ROSIDL_TYPESUPPORT_MICROXRCEDDS_C_H_
+#define MIRS_MSGS__MSG__BASIC_PARAM__ROSIDL_TYPESUPPORT_MICROXRCEDDS_C_H_
+
+
+#include <stddef.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include "rosidl_runtime_c/message_type_support_struct.h"
+#include "rosidl_typesupport_interface/macros.h"
+#include "mirs_msgs/msg/rosidl_typesupport_microxrcedds_c__visibility_control.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+ROSIDL_TYPESUPPORT_MICROXRCEDDS_C_PUBLIC_mirs_msgs
+size_t get_serialized_size_mirs_msgs__msg__BasicParam(
+  const void * untyped_ros_message,
+  size_t current_alignment);
+
+ROSIDL_TYPESUPPORT_MICROXRCEDDS_C_PUBLIC_mirs_msgs
+size_t max_serialized_size_mirs_msgs__msg__BasicParam(
+  bool * full_bounded,
+  size_t current_alignment);
+
+ROSIDL_TYPESUPPORT_MICROXRCEDDS_C_PUBLIC_mirs_msgs
+const rosidl_message_type_support_t *
+  ROSIDL_TYPESUPPORT_INTERFACE__MESSAGE_SYMBOL_NAME(rosidl_typesupport_microxrcedds_c, mirs_msgs, msg, BasicParam)();
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif  // MIRS_MSGS__MSG__BASIC_PARAM__ROSIDL_TYPESUPPORT_MICROXRCEDDS_C_H_
